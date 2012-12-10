c2d56390e4ee73178b31063e9cc6501f3f3116ff
Sylvain Giuliani <sgiuliani@gmail.com>
2012-12-10 23:44:36 +0100

    fix ticker

--- static/js/ticker.js
+++ static/js/ticker.js
@@ -50,3 +50,24 @@ function playerHiglight(){
 			$("#xtable tbody td[data-lineup*='"+playerId+"']").parent().css("background-color","");
 	});
 };
+
+
+//Hover playername highlight team
+// function playerHiglightH2h(){
+// 	$('#update li').hover(function(){
+// 		playerId = $(this).find('span').attr("pid");
+// 		$(".team").each(function(){
+// 			console.log($(this).attr('data-lineup').split(','))
+// 				lineup = $(this).attr('data-lineup').split(',')
+// 				console.log(lineup[0])
+
+
+
+// 		});
+
+// 		$("#h2h-table tbody td[lineup*='\'"+playerId+"\'']").css("background-color","#d9edf7");
+// 		},
+// 		function(){
+// 			$("#h2h-table tbody td[lineup*='\'"+playerId+"\'']").css("background-color","");
+// 	});
+};
